0cd50aa24296f26f5ae2d1c4b27b18428b34d4ed
Pu <pubary@proton.me>
2023-03-21 22:10:56 +0300

    added parser

--- pereval/views.py
+++ pereval/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.exceptions import APIException
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -54,23 +55,18 @@ class PerevalViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericVi
 
     @action(methods=['post'], detail=False)
     def submitData(self, request):
-        # try:
-        #     serializer = SubmitDataSerializer(data=request.data)
-        #     # parser_classes = (MultiPartParser, FormParser,)
-        #     if serializer.is_valid(raise_exception=True):
-        #         serializer.save()
-        #         pk = serializer.data['pk']
-        #         return Response({'status': 200, 'message': None, 'id': pk})
-        # except APIException as exc:
-        #     if exc.status_code == 400:
-        #         return Response({'status': exc.status_code, 'message': ' Bad Request', 'id': None})
-        #     else:
-        #         return Response({'status': exc.status_code, 'message': 'Ошибка подключения к базе данных', 'id': None})
-        serializer = SubmitDataSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            pk = serializer.data['pk']
-            return Response({'status': 200, 'message': None, 'id': pk})
+        try:
+            serializer = SubmitDataSerializer(data=request.data)
+            parser_classes = (MultiPartParser, FormParser,)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                pk = serializer.data['pk']
+                return Response({'status': 200, 'message': None, 'id': pk})
+        except APIException as exc:
+            if exc.status_code == 400:
+                return Response({'status': exc.status_code, 'message': ' Bad Request', 'id': None})
+            else:
+                return Response({'status': exc.status_code, 'message': 'Ошибка подключения к базе данных', 'id': None})
 
 
 def redirect_to_api(request):
